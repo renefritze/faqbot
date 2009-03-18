21e49ce141f62039563cd4706fa75fdbe7dadbae
Rene Milk <koshi@springlobby.info>
2009-03-18 01:46:43 +0100

    add leave chan

--- join_channels.py
+++ join_channels.py
@@ -10,10 +10,13 @@ class Main:
 		cmns = parselist(self.app.config["channels"],',')
 		socket.send("JOIN %s\n" % (cmns[0]))
 		self.joined_channels = 1
-	    if command == "SAID" and len(args) > 3 and args[2] == "!faqchan" and args[1] in self.admins:
+	    if command == "SAID" and len(args) > 3 and args[1] in self.admins:
 		for chan in args[3:]:
-		    socket.send("JOIN %s\n" % (chan))
-		    print ("JOIN %s\n" % (chan))
+		    if args[2] == "!faqchan":
+			socket.send("JOIN %s\n" % (chan))
+		    else if args[2] == "!faq!chan":
+			socket.send("LEAVE %s\n" % (chan))
+		    
 	def onload(self,tasc):
 	    self.app = tasc.main
 	    self.admins = parselist(self.app.config["admins"],',')
