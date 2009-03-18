eea9b1bdf712a14c0d11188bdbbc6bb5a00e770e
Rene Milk <koshi@springlobby.info>
2009-03-18 01:08:18 +0100

    remove unnecessary code bit

--- join_channels.py
+++ join_channels.py
@@ -3,13 +3,14 @@ import string
 from utilities import *
 class Main:
 	joined_channels = 0
-	def getFAQ(self,key,socket):
-	    socket.send("SAYPRIVATE _koshi_007 %s\n" % (key))
-
+	admins = []
+	
 	def oncommandfromserver(self,command,args,socket):
 	    if self.joined_channels != 1:
 		cmns = parselist(self.app.config["channels"],',')
 		socket.send("JOIN %s\n" % (cmns[0]))
 		self.joined_channels = 1
+	    #if command == "SAID" and len(args) > 2 and args[2] == !channel
 	def onload(self,tasc):
-	  self.app = tasc.main
+	    self.app = tasc.main
+	    self.admins = parselist(self.app.config["admins"],',')
