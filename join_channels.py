cd40ce0278c990d718e791a640c235b89f1e8e15
Andrea Piras <braindamage@springlobby.info>
2010-06-26 18:57:25 +0200

    now join channels plugin saves channel list

--- join_channels.py
+++ join_channels.py
@@ -4,21 +4,34 @@ from utilities import *
 class Main:
 	joined_channels = 0
 	admins = []
-	
+	channels = []
+
 	def onloggedin(self,socket):
-		cmns = parselist(self.app.config["channels"],',')
-		for chan in cmns :
+		for chan in self.cmns :
 			socket.send("JOIN %s\n" % (chan))
 		self.joined_channels = 1
-	
+
 	def oncommandfromserver(self,command,args,socket):
 	    if command == "SAID" and len(args) > 3 and args[1] in self.admins:
 		for chan in args[3:]:
-		    if args[2] == "!faqchan":
-			socket.send("JOIN %s\n" % (chan))
-		    if args[2] == "!faq!chan":
-			socket.send("LEAVE %s\n" % (chan))
+			if args[2] == "!faqchan":
+				socket.send("JOIN %s\n" % (chan))
+				if not chan in self.channels:
+					self.channels.append(chan)
+					self.saveChannels()
+			if args[2] == "!faq!chan":
+				socket.send("LEAVE %s\n" % (chan))
+				if chan in self.channels:
+					self.channels.remove(chan)
+					self.saveChannels()
+
+	def saveChannels(self):
+		savestring = ""
+		for channel in self.channels:
+			savestring += channel + ","
+		self.app.config["channels"] = savestring
 
 	def onload(self,tasc):
 	    self.app = tasc.main
 	    self.admins = parselist(self.app.config["admins"],',')
+	    self.channels = parselist(self.app.config["channels"],',')
