576b7b9c20fd2c5471d325b76dea3d5d83d53c08
Andrea Piras <braindamage@springlobby.info>
2010-06-26 19:12:16 +0200

    typo

--- join_channels.py
+++ join_channels.py
@@ -30,7 +30,7 @@ class Main:
 		for channel in self.channels:
 			savestring += channel + ","
 		self.app.config["channels"] = savestring
-		writeconfigfile("Main.cfg",self.app.config)
+		writeconfigfile("Main.conf",self.app.config)
 
 	def onload(self,tasc):
 	    self.app = tasc.main
